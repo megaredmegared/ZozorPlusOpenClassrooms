2938dfb1d831f2bc197c922a38db4bf2290bafe5
megaredmegared <sarseb@gmail.com>
2018-12-24 09:38:48 +0100

    test commit

--- Zozor+/ViewController.swift
+++ Zozor+/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - Properties
+    // MARK: - Properties test commit
     var stringNumbers: [String] = [String()]
     var operators: [String] = ["+"]
     var index = 0
+    
     var isExpressionCorrect: Bool {
         if let stringNumber = stringNumbers.last {
             if stringNumber.isEmpty {
